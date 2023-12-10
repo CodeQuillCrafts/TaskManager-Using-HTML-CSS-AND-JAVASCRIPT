1feb80b6ea0e4f6ae5d8003998215eed307be828
Shivam Gupta <codequillcrafts@gmail.com>
2023-12-10 14:59:16 +0530

    adding files

--- app.js
+++ app.js
@@ -1,32 +1,62 @@
-document.addEventListener('DOMContentLoaded', function () {
-    let setterBtn = document.getElementsByClassName('setterBtn')[0];
-    let setterInput = document.getElementsByClassName('setterInput')[0];
-    let getter = document.getElementsByClassName('getter')[0]
-    setterBtn.addEventListener('click', function (e) {
-        e.preventDefault();
-        console.log("clicked");
-        if (setterInput.value == '') {
-            console.log("Empty");
-        }
-        else {
-            let newItem = document.createElement('div');
-            newItem.classList.add('setter');
-            let newItemInput = document.createElement('p');
-            newItemInput.textContent = setterInput.value;
-            let icons = document.createElement('div');
-            icons.classList.add("icons");
-            let iFst = document.createElement('i');
-            iFst.classList.add('fa');
-            iFst.classList.add('fa-pencil');
-            let iScnd = document.createElement('i');
-            iScnd.classList.add('fa');
-            iScnd.classList.add('fa-trash');
+let setterBtn = document.getElementsByClassName('setterBtn')[0];
+let setterInput = document.getElementsByClassName('setterInput')[0];
+let getter = document.getElementsByClassName('getter')[0];
 
-            icons.appendChild(iFst);
-            icons.appendChild(iScnd);
-            newItem.appendChild(newItemInput);
-            newItem.appendChild(icons);
-            getter.appendChild(newItem);
-        }
+loadTasks();
+setterBtn.addEventListener('click', addTask);
+
+function addTask() {
+    if (setterInput.value == '') {
+        console.log("Empty");
+    } else {
+        let newItem = document.createElement('div');
+        newItem.classList.add('setter');
+        let newItemInput = document.createElement('p');
+        newItemInput.textContent = setterInput.value;
+        let icons = document.createElement('div');
+        icons.classList.add("icons");
+        let iFst = document.createElement('i');
+        iFst.classList.add('fa');
+        iFst.classList.add('fa-pencil');
+        let iScnd = document.createElement('i');
+        iScnd.classList.add('fa');
+        iScnd.classList.add('fa-trash');
+
+        icons.appendChild(iFst);
+        icons.appendChild(iScnd);
+        newItem.appendChild(newItemInput);
+        newItem.appendChild(icons);
+        getter.appendChild(newItem);
+
+        saveTask(setterInput.value);
+    }
+}
+function saveTask(task) {
+    let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    tasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function loadTasks() {
+    let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    tasks.forEach(function (task) {
+        let newItem = document.createElement('div');
+        newItem.classList.add('setter');
+        let newItemInput = document.createElement('p');
+        newItemInput.textContent = task;
+        let icons = document.createElement('div');
+        icons.classList.add("icons");
+        let iFst = document.createElement('i');
+        iFst.classList.add('fa');
+        iFst.classList.add('fa-pencil');
+        let iScnd = document.createElement('i');
+        iScnd.classList.add('fa');
+        iScnd.classList.add('fa-trash');
+
+        icons.appendChild(iFst);
+        icons.appendChild(iScnd);
+        newItem.appendChild(newItemInput);
+        newItem.appendChild(icons);
+        getter.appendChild(newItem);
     });
-});
+}
